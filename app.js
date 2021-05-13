cc9ce58197c355fe55359f07e7894a7d8afcd58c
Alvin Cousins <alvin.cousins@googlemail.com>
2021-05-13 12:39:11 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const itemRouter = require("./routes/itemRouter");
+
+const app = express();
+
+// MIDDLEWARE
+
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  console.log("Hello from the middleware");
+});
+
+// ROUTES
+app.use("/api/v1/items", itemRouter);
+
+// START SERVER
+module.exports = app;
